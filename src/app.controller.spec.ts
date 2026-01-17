39054f93114478f23b3d6ff55eda3efd85c71455
Serhii Zelik <40599524+ZelikSV@users.noreply.github.com>
2026-01-17 14:20:14 +0200

    updated README

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return correct data', () => {
+      expect(appController.getHello()).toBe('You are on root path');
     });
   });
 });
