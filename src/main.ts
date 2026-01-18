46618c40ae62e854d194ba61750fb14253f3dfcc
Serhii Zelik <40599524+ZelikSV@users.noreply.github.com>
2026-01-18 11:37:37 +0200

    changed logger

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
+import { Logger } from '@nestjs/common';
 
 import { AppModule } from './app.module';
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
+  const logger = new Logger('Server');
   const configService = app.get(ConfigService);
   const port = configService.get<number>('PORT', 3000);
 
   await app.listen(port);
 
-  // eslint-disable-next-line no-console
-  console.log(`Application is running on: http://localhost:${port}`);
+  logger.log(`🚀Application is running on: http://localhost:${port}`);
 }
 
 void bootstrap();
